85e37c61bd17ac9542835fac5c45c41b2e42f0db
juggler39 <juggler39@yandex.ru>
2020-06-29 19:05:59 +0200

    add playfield

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 let mainDiv=document.createElement ('div');
 mainDiv.id='flex-container';
 document.body.appendChild(mainDiv);
+for (let y=0;y<16;y++) {
+    for (let x=0;x<16;x++) {
+        let innerDiv=document.createElement ('div');
+        innerDiv.id=`x${x}y${y}`;
+        mainDiv.appendChild(innerDiv);
+        innerDiv.innerHTML=x;
+    }
+}
